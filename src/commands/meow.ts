452dd2f6f15a0a1acf7ab12468e3bfc740d31436
Lionel <mottet.lionel@gmail.com>
2020-07-31 16:52:15 +0200

    wip

--- src/commands/meow.ts
+++ src/commands/meow.ts
@@ -1,9 +1,7 @@
-import { botCache } from "../../mod.ts";
+import { botCache } from "../../mod.ts"
+import { sendMessage } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/handlers/channel.ts"
 
-botCache.commands.set('meow', {
-    callback: (message) => {
-        return message.channel.sendMessage(
-            'MEOWWW!!!',
-        );
-    },
-});
+botCache.commands.set("meow", {
+  name: `meow`,
+  execute: (message) => sendMessage(message.channel, "MEOWWW!!!"),
+})
