cc3f3180b0198592d18394bbc9ef9a1b2e734113
Lionel <mottet.lionel@gmail.com>
2020-08-02 12:14:15 +0200

    wip

--- deno_dir/gen/file/D/Code/Ninie.io/deps.ts.js
+++ deno_dir/gen/file/D/Code/Ninie.io/deps.ts.js
@@ -1,10 +1,11 @@
 export { hasChannelPermission, sendMessage, } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/handlers/channel.ts";
+export { getMember } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/handlers/guild.ts";
 export { avatarURL, kick, } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/handlers/member.ts";
 export { deleteMessage } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/handlers/message.ts";
-export { botID } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/module/client.ts";
+export { botID, createClient as Client, } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/module/client.ts";
 export { Errors } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/types/errors.ts";
+export { Intents, } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/types/options.ts";
 export { Permissions, } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/types/permission.ts";
 export { cache } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/utils/cache.ts";
 export { logger } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/utils/logger.ts";
-export { getMember } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/handlers/guild.ts";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGVwcy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImRlcHMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxFQUNMLG9CQUFvQixFQUNwQixXQUFXLEdBQ1osTUFBTSxzRkFBc0YsQ0FBQztBQUM5RixPQUFPLEVBQ0wsU0FBUyxFQUNULElBQUksR0FDTCxNQUFNLHFGQUFxRixDQUFDO0FBQzdGLE9BQU8sRUFBRSxhQUFhLEVBQUUsTUFBTSxzRkFBc0YsQ0FBQztBQUNySCxPQUFPLEVBQUUsS0FBSyxFQUFFLE1BQU0sbUZBQW1GLENBQUM7QUFNMUcsT0FBTyxFQUFFLE1BQU0sRUFBRSxNQUFNLGtGQUFrRixDQUFDO0FBTzFHLE9BQU8sRUFFTCxXQUFXLEdBQ1osTUFBTSxzRkFBc0YsQ0FBQztBQUM5RixPQUFPLEVBQUUsS0FBSyxFQUFFLE1BQU0saUZBQWlGLENBQUM7QUFDeEcsT0FBTyxFQUFFLE1BQU0sRUFBRSxNQUFNLGtGQUFrRixDQUFDO0FBQzFHLE9BQU8sRUFBRSxTQUFTLEVBQUUsTUFBTSxvRkFBb0YsQ0FBQyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGVwcy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImRlcHMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxFQUNMLG9CQUFvQixFQUNwQixXQUFXLEdBQ1osTUFBTSxzRkFBc0YsQ0FBQztBQUM5RixPQUFPLEVBQUUsU0FBUyxFQUFFLE1BQU0sb0ZBQW9GLENBQUM7QUFDL0csT0FBTyxFQUNMLFNBQVMsRUFDVCxJQUFJLEdBQ0wsTUFBTSxxRkFBcUYsQ0FBQztBQUM3RixPQUFPLEVBQUUsYUFBYSxFQUFFLE1BQU0sc0ZBQXNGLENBQUM7QUFDckgsT0FBTyxFQUNMLEtBQUssRUFDTCxZQUFZLElBQUksTUFBTSxHQUN2QixNQUFNLG1GQUFtRixDQUFDO0FBTTNGLE9BQU8sRUFBRSxNQUFNLEVBQUUsTUFBTSxrRkFBa0YsQ0FBQztBQU8xRyxPQUFPLEVBRUwsT0FBTyxHQUNSLE1BQU0sbUZBQW1GLENBQUM7QUFDM0YsT0FBTyxFQUVMLFdBQVcsR0FDWixNQUFNLHNGQUFzRixDQUFDO0FBQzlGLE9BQU8sRUFBRSxLQUFLLEVBQUUsTUFBTSxpRkFBaUYsQ0FBQztBQUN4RyxPQUFPLEVBQUUsTUFBTSxFQUFFLE1BQU0sa0ZBQWtGLENBQUMifQ==
