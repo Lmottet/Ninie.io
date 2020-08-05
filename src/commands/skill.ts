c21c2dbe8cc6a55e21e7a41f271c9005fa4c3ed6
Lionel <mottet.lionel@gmail.com>
2020-08-05 15:33:15 +0200

    wip

--- src/commands/skill.ts
+++ src/commands/skill.ts
@@ -1,4 +1,3 @@
-import { axiod } from "../../deps.ts";
 import { botCache } from "../../mod.ts";
 
 botCache.commands.set("skill", {
@@ -8,14 +7,10 @@ botCache.commands.set("skill", {
   },
 });
 
-const getRaiderIo = (realm: string, name: string) => {
-  return axiod
-    .get("https://raider.io/api/v1/characters/profile", {
-      params: {
-        region: "EU",
-        realm: realm,
-        name: name,
-        fields: "%20mythic_plus_scores_by_season%3Acurrent",
-      },
-    }).then((response: any) => console.log(response));
-};
+async function getRaiderIo(realm: string, name: string) {
+  const res = await fetch(
+    `https://raider.io/api/v1/characters/profile?region=eu&realm=${realm}&name=${name}&fields=%20mythic_plus_scores_by_season%3Acurrent`,
+  );
+  const data = await res.json();
+  console.log(data);
+}
