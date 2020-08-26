b38d4251a10fbfe8ecc32985033fb4e9cbed0ce4
Lionel <mottet.lionel@gmail.com>
2020-08-26 23:44:10 +0200

    binaries

--- deps.ts
+++ deps.ts
@@ -25,4 +25,4 @@ export {
   sendMessage,
 } from "https://x.nest.land/Discordeno@8.0.0/mod.ts";
 
-export { serve } from "https://deno.land/std@v0.24.0/http/server.ts";
+export { serve } from "https://deno.land/std@0.66.0/http/server.ts";
