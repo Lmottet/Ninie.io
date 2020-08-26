4a7057174bc08aeb40de087f722b79553f2df9a7
Lionel <mottet.lionel@gmail.com>
2020-08-27 01:50:29 +0200

    try setTimeoutCeption

--- mod.ts
+++ mod.ts
@@ -58,14 +58,17 @@ await Promise.all(
 
 const wakeUpDyno = (url: string, interval = 25) => {
   const milliseconds = interval * 60000;
-  setInterval(() => {
-    try {
-      // HTTP GET request to the dyno's url
-      fetch(url).then(() => console.log(`Fetching ${url}.`));
-    } catch (err) { // catch fetch errors
-      console.log(`Error fetching ${url}: ${err.message} 
-            Will try again in ${interval} minutes...`);
-    }
+  setTimeout(() => {
+    setTimeout(() => {
+      try {
+        // HTTP GET request to the dyno's url
+        fetch(url).then(() => console.log(`Fetching ${url}.`));
+      } catch (err) { // catch fetch errors
+        console.log(
+          `Error fetching ${url}: ${err.message} Will try again in ${interval} minutes...`,
+        );
+      }
+    }, 1);
   }, milliseconds);
 };
 
