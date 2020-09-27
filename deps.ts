2da635df12c2fd80b26d44a1227b3602fa203a03
Lionel <mottet.lionel@gmail.com>
2020-09-27 10:11:48 +0200

    deno version update

--- deps.ts
+++ deps.ts
@@ -23,6 +23,6 @@ export {
   Permission,
   Permissions,
   sendMessage,
-} from "https://x.nest.land/Discordeno@8.0.0/mod.ts";
+} from "https://x.nest.land/Discordeno@9.0.1/mod.ts";
 
-export { serve } from "https://deno.land/std@0.66.0/http/server.ts";
+export { serve } from "https://deno.land/std@0.70.0/http/server.ts";
